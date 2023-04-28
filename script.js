f4de9b3c5ce669a171d148f0289af9575bbbef97
Aldo Miralles <alditomiralles3@gmail.com>
2023-04-28 15:09:52 +0200

    Creating the SectionOne

--- script.js
+++ script.js
@@ -27,3 +27,26 @@ window.onscroll = function () {
     }
     mainLocation = actualScrolling;
 }
+// Sliding background img
+const imgArray = [
+    "url(img/img-1.jpg)",
+    "url(img/img-2.jpg)"
+]
+
+let bestSalesOne = document.getElementById("bestSalesOne");
+
+let i = 0;
+
+const slideShow = () => {
+    bestSalesOne.style.backgroundImage = imgArray[i];
+    if (i < imgArray.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    // i = (i < imgArray.length - 1) ? i+1 : 0;
+}
+
+setInterval(slideShow, 6000)
+
+
